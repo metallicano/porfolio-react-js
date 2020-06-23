0a64682d6abfb6823077392cec44cd442f013e93
daniel <danielmtgiraldo@gmail.com>
2020-06-23 15:17:32 -0500

    Updates

---
+++ precache-manifest.41d3a7fb2ae0a9510d357c90a51a9f8a.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e3d27c34f0e4ee79abf23d229612568",
+    "url": "/github-pages-daniel/index.html"
+  },
+  {
+    "revision": "0f058f12d47695ed69cf",
+    "url": "/github-pages-daniel/static/css/main.c8b40327.chunk.css"
+  },
+  {
+    "revision": "5cc32cc50b4f6664c702",
+    "url": "/github-pages-daniel/static/js/2.9ce65e10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-pages-daniel/static/js/2.9ce65e10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f058f12d47695ed69cf",
+    "url": "/github-pages-daniel/static/js/main.342b8a81.chunk.js"
+  },
+  {
+    "revision": "7513d198f129ba428d60",
+    "url": "/github-pages-daniel/static/js/runtime-main.3a58d5bb.js"
+  },
+  {
+    "revision": "7384412324c769e447d3c4f86e9ff068",
+    "url": "/github-pages-daniel/static/media/bot2.73844123.png"
+  },
+  {
+    "revision": "7a6eda99b24f50ae4376906289c2e368",
+    "url": "/github-pages-daniel/static/media/daniel5.7a6eda99.jpg"
+  },
+  {
+    "revision": "e9058c9f40c0a4ce2399bb9b2ae0a634",
+    "url": "/github-pages-daniel/static/media/github.e9058c9f.png"
+  },
+  {
+    "revision": "7a864f07681f187fb572468bfc949977",
+    "url": "/github-pages-daniel/static/media/hub2.7a864f07.png"
+  },
+  {
+    "revision": "6a12c9813b2c41582357005e60f16df4",
+    "url": "/github-pages-daniel/static/media/insta2.6a12c981.png"
+  },
+  {
+    "revision": "266877445b54dde36b5ce02dfbd564ba",
+    "url": "/github-pages-daniel/static/media/iot3.26687744.png"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/github-pages-daniel/static/media/js2.2d2e31cc.svg"
+  },
+  {
+    "revision": "780d570f8226161278208c4976fec02f",
+    "url": "/github-pages-daniel/static/media/lol5.780d570f.png"
+  },
+  {
+    "revision": "bca7dfdc5274e4cf0eb951adaa1e4993",
+    "url": "/github-pages-daniel/static/media/mongo2.bca7dfdc.png"
+  },
+  {
+    "revision": "409b9927755c1eb364c84380d1db434a",
+    "url": "/github-pages-daniel/static/media/node3.409b9927.png"
+  },
+  {
+    "revision": "26d6561a4cb844bbf445ab455bac65d0",
+    "url": "/github-pages-daniel/static/media/pokemon.26d6561a.jpg"
+  },
+  {
+    "revision": "ed3410f9da336945deeea7e9f63cde81",
+    "url": "/github-pages-daniel/static/media/ps.ed3410f9.png"
+  },
+  {
+    "revision": "b9bbb1d722d3ef6ebf7d573703b5925e",
+    "url": "/github-pages-daniel/static/media/python2.b9bbb1d7.jpg"
+  },
+  {
+    "revision": "06aae9a247129d1b39fe16badf2f427f",
+    "url": "/github-pages-daniel/static/media/rasp-berry.06aae9a2.png"
+  },
+  {
+    "revision": "42c6958e221d4db49ffe5dac7366f5d8",
+    "url": "/github-pages-daniel/static/media/rasp3.42c6958e.png"
+  },
+  {
+    "revision": "d14e4fd3c8b856f05612643d6773e497",
+    "url": "/github-pages-daniel/static/media/react.d14e4fd3.png"
+  },
+  {
+    "revision": "59c467536111a981d904a050d335c2be",
+    "url": "/github-pages-daniel/static/media/redux.59c46753.png"
+  },
+  {
+    "revision": "1674e9af4aab3739a029f4e71a88de75",
+    "url": "/github-pages-daniel/static/media/weather.1674e9af.png"
+  },
+  {
+    "revision": "a754866ad21387dbc35ca2b0070bd171",
+    "url": "/github-pages-daniel/static/media/ws2.a754866a.png"
+  }
+]);
